9720306915d21c24dacecbed99eb5cdaecb71826
Pramira Herath <pramira.herath@haycarb.com>
2026-02-27 09:33:47 +0530

    weight conv

--- unit_converter.py
+++ unit_converter.py
@@ -12,4 +12,18 @@ def length_converter():
         print(f"{value} Miles = {value / 0.621371} KM")
 
 
-length_converter()
+def weight_converter():
+    print("\n Weight Converter")
+    print("1. KG to Pounds")
+    print("2. Pounds to KG")
+
+    choice = input("Choose : ")
+    value = float(input("Input Value : "))
+
+    if choice == "1":
+        print(f"{value} KG = {value * 2.20462} Pounds\n")
+    elif choice == "2":
+        print(f"{value} Pounds = {value / 2.2046} KG")
+
+
+weight_converter()
